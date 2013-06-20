2305da1258b41469623bf5cf8c008723e28f14db
Austin Lin <austin@austinylin.com>
2013-06-19 20:28:40 -0500

    first pass at kap deploy hook

--- alf.rb
+++ alf.rb
@@ -1,3 +1,11 @@
-require "rubygems"
-require "sinatra"
+require 'rubygems'
+require 'sinatra'
+require 'json'
 
+get '/clients/kathyaperkins/hooks/deploy/:env' do
+  if env[:params] == 'stage'
+    `cd ~/ClientWork/kathyaperkins.com && git pull`
+    content_type :json
+    { status: 'ok' }.to_json
+  end
+end
